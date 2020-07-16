d06b1bcbd2f46ec92ed15a3b2faaeff7930a1e76
Mandar <mandar242@gmail.com>
2020-07-16 12:18:21 -0700

    interactive background using particles.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-
+import Particles from 'react-particles-js';
 import './App.css';
+
+const paticlesOptions = {
+  particles: {
+    number:{
+      value: 70,
+      density:{
+        enable: true,
+        value_area: 500
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className = 'particles'
+              params={paticlesOptions}
+              style={{
+                width: '100%',
+                //backgroundImage: `url(${logo})` 
+              }}
+            />
       <Navigation />
       <Logo />
       <Rank/>
