25ac0a6913c86c3d3cd24d6e9db748d55f23130c
sharvarikhedkar <anujakhedkar96@gmail.com>
2020-07-16 18:08:33 -0700

    face detection complete

--- src/App.js
+++ src/App.js
@@ -32,9 +32,39 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl:''
+      imageUrl:'',
+      box: {},
     }
   }
+
+  calcuateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;//grabing the first bounding box 
+    //dom manipulation
+    const image = document.getElementById('inputImage');
+    //remember bounding box is the percentage of this image 
+    const width = Number(image.width)//to make sure that its a number
+    const height = Number(image.height)
+    console.log(width,height);
+
+    return{
+      //letcol is the percentage of the width 
+      leftCol: clarifaiFace.left_col * width,
+      topRow:clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width), //is the total percentage - the width from the left hand side 
+      bottomRow: height - (clarifaiFace.bottom_row * height) 
+
+      //return a box here ,which will fill the job, here we will first need to figure out the fist second third and fourth dot sourrounding the box and then just wrap it in an order 
+    }
+
+  }
+  //this is going to recieve a return value 
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box:box});
+  } 
+
+
+  //depending on the reion we get arrays, so if we have multiple people then we'll get multiple arrays 
 
   onInputChange = (event) =>{
     this.setState({input: event.target.value})
@@ -42,15 +72,15 @@ class App extends Component {
 
   onButtonSubmit = () =>{
     this.setState({imageUrl:this.state.input})
-      app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
+      app.models
+        .predict(
+          Clarifai.FACE_DETECT_MODEL,
+          this.state.input)
+          //calculates inner function and displays facebox 
+        .then(response => this.displayFaceBox(this.calcuateFaceLocation(response))) //this will get the response which is the bounding box, but this response will be recieved what we console log last time 
+        .catch(err => console.log(err));
         // do something with response
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+        //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
   }
 
   render(){
@@ -67,7 +97,7 @@ class App extends Component {
       <Logo />
       <Rank/>
       <ImageLinkForm onInputChange= {this.onInputChange} onButtonSubmit ={this.onButtonSubmit} /> 
-      <FaceRecognition imageUrl={this.state.imageUrl}/>
+      <FaceRecognition box = {this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
   )
   }
