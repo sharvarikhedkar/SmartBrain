2c6f9ef4cd48ea13dde923debacab31d5d7bed11
sharvarikhedkar <anujakhedkar96@gmail.com>
2020-07-17 16:21:21 -0700

    register signout routes complete

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -10,22 +10,22 @@ const Signin = ({onRouteChange}) => {
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                 <legend className="f4 fw6 ph0 mh0">Sign In</legend>
                 <div className="mt3">
-                <label className="db fw6 lh-copy f6" for="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white shadow-5 w-100" type="email" name="email-address"  id="email-address" />
                 </div>
                 <div className="mv3">
-                <label className="db fw6 lh-copy f6" for="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input className="b pa2 br-2 input-reset ba bg-transparent hover-bg-black hover-white shadow-5 w-100" type="password" name="password"  id="password" />
                 </div>
             </fieldset>
             <div className="">
-                <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                <input className="b ph3 pv2 input-reset ba bg-transparent grow pointer f6 dib" 
                 type="submit" value="Sign in" 
                 onClick = {()=>onRouteChange('home')}
                 />
             </div>
             <div className="lh-copy mt3">
-                <a href="#0" className="f6 link dim black db">Register</a>
+                <p onClick = {()=>onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
             </form>
         </main>
