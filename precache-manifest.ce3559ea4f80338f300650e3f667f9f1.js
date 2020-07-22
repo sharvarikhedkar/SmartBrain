d3f32ba242c8cfbd52266bf152d5e0ad8c9ab652
Mandar <mandar242@gmail.com>
2020-07-21 23:29:41 -0700

    Updates

---
+++ precache-manifest.ce3559ea4f80338f300650e3f667f9f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4108b47543607c055f27ce6ab839b3d8",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "ee9237d7cd03467fec0d",
+    "url": "/SmartBrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "db7c41ed909353aedb8b",
+    "url": "/SmartBrain/static/css/main.69f74e22.chunk.css"
+  },
+  {
+    "revision": "ee9237d7cd03467fec0d",
+    "url": "/SmartBrain/static/js/2.dba7963f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/SmartBrain/static/js/2.dba7963f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7c41ed909353aedb8b",
+    "url": "/SmartBrain/static/js/main.44af4fc3.chunk.js"
+  },
+  {
+    "revision": "58a3c1ed602e0e639edd",
+    "url": "/SmartBrain/static/js/runtime-main.f4d9b11e.js"
+  }
+]);
